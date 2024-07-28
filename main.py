0fb0688fee3444f46d6e4004cf6572d3b2398576
adminl <arjunamin2011@gmail.com>
2024-07-28 04:02:36 -0400

    used "input function"

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
-name = int('Pleas enter your name: ')
-print("Hello," + name)
+name = input("enter your name: ") #used function called "input" to ask for user's name
+print("Hello," + name) #Printed Hello message with input variable.
