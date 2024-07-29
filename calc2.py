d1476ca822a35fca6e879ee03df376d62aa9a12a
adminl <arjunamin2011@gmail.com>
2024-07-29 02:58:58 -0400

    Integer calculator2

---
+++ calc2.py
@@ -0,0 +1,27 @@
+## Learning Integer
+
+"""Example 1 of calc with variable input
+# ask for variable value
+x = input("what is x? ")
+y = input("What is y? ")
+#used integer for calculate numbers
+z= int(x) + int(y)
+#Printans
+print(z)
+"""
+# ----------------
+
+# Example 2 Calculator with integer and input
+# ask for input with integer
+x = int(input("what is x? "))
+y = int(input("What is y? "))
+# print integer ans
+print(x * y)
+
+
+
+
+
+
+
+
