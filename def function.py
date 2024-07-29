db8a32b74b8951c12735540ca1d29f9c61b52f15
adminl <arjunamin2011@gmail.com>
2024-07-29 05:11:39 -0400

    defined function

--- def function.py
+++ def function.py
@@ -1,3 +1,11 @@
-def greet(name):
+# Information can be passed into functions as arguments.
+# Arguments are specified after the function name, inside the parentheses.
+# You can add as many arguments as you want, just separate them with a comma.
+# The following example has a function with one argument (firstname).
+# When the function is called, we pass along a first name, which is used inside the function to print the full name:
+def myfunc(fname):
+  print(fname + " Amin")
 
-    print(f"Hello, {name}!")
+myfunc("Arjun")
+myfunc("Saloni")
+myfunc("Hreem")
