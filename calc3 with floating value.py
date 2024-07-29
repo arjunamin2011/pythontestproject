3813234f9c8a86c41786ef1d4c680d473a05d20a
adminl <arjunamin2011@gmail.com>
2024-07-29 03:33:10 -0400

    calc using float 1

---
+++ calc3 with floating value.py
@@ -0,0 +1,8 @@
+# Learning Float : Float is numbers with decimal data like e.g: .001, 10.234, -1.23
+
+# Example 2 Calculator with float and input
+# Ask for input with float value
+x = float(input("what is x? "))
+y = float(input("What is y? "))
+# printed integer ans
+print(x % y)
