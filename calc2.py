16db9ca48363d6349cbe568ec3c231e872522804
adminl <arjunamin2011@gmail.com>
2024-07-29 03:13:36 -0400

    Integer calculator 2

--- calc2.py
+++ calc2.py
@@ -1,22 +1,12 @@
-#Learning Integer: Integer is numbers without desimal like e.g: -1,0,1,2,100
+# Learning Integer: Integer is numbers without desimal like e.g: -1,0,1,2,100
 
-## Example 1 of calc with variable input
-## ask for variable value
-#x = input("what is x? ")
-#y = input("What is y? ")
-## used integer for calculate numbers
-#z= int(x) + int(y)
-## Printans
-#print(z)
-#--------------------#
+# Example 2 Calculator with integer and input
+# Ask for input with integer
+x = int(input("what is x? "))
+y = int(input("What is y? "))
+# printed integer ans
+print(x * y)
 
-## Example 2 Calculator with integer and input
-##Ask for input with integer
-#x = int(input("what is x? "))
-#y = int(input("What is y? "))
-## printed integer ans
-#print(x * y)
-#----------------#
 
 
 
